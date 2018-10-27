dcd6958b66e18c7d46605694455c494296b2957f
Kadeem Palacios <kadeempalacios@gmail.com>
2018-10-27 16:22:31 -0700

    Initial Commit

---
+++ ThriveCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThriveCodeChallenge
+//
+//  Created by Kadeem Palacios on 10/27/18.
+//  Copyright © 2018 Kadeem Palacios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
